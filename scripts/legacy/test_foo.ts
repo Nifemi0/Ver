70fa226dcbdfac2b9370565ff21f864940ea6066
Nifemi0 <nifemi0@users.noreply.github.com>
2026-07-18 15:58:31 +0000

    chore: remove legacy registry scripts

--- scripts/legacy/test_foo.ts
+++
@@ -1,8 +0,0 @@
-export const HashGraphZodSchema = z.object({
-  metadata: MetadataSchema,
-  statistics: StatisticsSchema,
-  structural: StructuralSchema,
-  semantic: SemanticSchema,
-  security: SecuritySchema,
-  developer: DeveloperSchema,
-});
